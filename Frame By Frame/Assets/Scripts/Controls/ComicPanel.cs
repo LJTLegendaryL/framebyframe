c909cf82035ec7188ff7ea13837561d6755d03df
S1LENTGHO5T <l39end4ry.l@gmail.com>
2023-04-30 18:39:47 -0700

    Panel Controls
    Panel Swapping, Placing, and Slot Checking

---
+++ Frame By Frame/Assets/Scripts/Controls/ComicPanel.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ComicPanel : MonoBehaviour
+{
+	public PanelRack panelRack;
+	public int panelIndex;
+}
--- Frame By Frame/Assets/Scripts/Controls/MouseDragging.cs
+++ Frame By Frame/Assets/Scripts/Controls/MouseDragging.cs
@@ -2,33 +2,47 @@ using UnityEngine;
 
 public class MouseDragging : MonoBehaviour
 {
+	public PanelRack panelRack;
 	public Camera mainCamera;
 	public LayerMask interactableMask;
 
 	public Transform selectedObject;
+	public bool isSwapping;
+	
 	bool selectableObjectInRange;
 	Vector2 mousePoint;
+	
+	public bool GetSelectionState()
+	{
+		return selectedObject != null;
+	}
 
 	void Update()
 	{
 		CalculateMousePosition();
 
-		if(selectedObject != null)
+		if(GetSelectionState())
 			selectedObject.position = Vector3.Lerp(selectedObject.position, mousePoint, 10 * Time.deltaTime);
 	}
 	
 	void CalculateMousePosition()
 	{
-		mousePoint = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-		RaycastHit2D hitData2D = Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, interactableMask);
-		
-		if(selectedObject == null)
+		if(!GetSelectionState())
 		{
-			if(Input.GetMouseButtonDown(0) && hitData2D.transform != null)
-				selectedObject = hitData2D.transform;
+			mousePoint = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hitData2D = Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, interactableMask);
+			
+			if(hitData2D.transform != null)
+			{
+				var panelComponent = hitData2D.transform.gameObject.GetComponent<ComicPanel>();
+				panelRack.SetPanelIndex(panelComponent.panelIndex);
+
+				if(Input.GetMouseButtonDown(0))
+					selectedObject = hitData2D.transform;
+			}
 		}
 		else
-			if(Input.GetMouseButtonDown(0))
+			if(Input.GetMouseButtonDown(0) && !isSwapping)
 				selectedObject = null;
 	}
 	
