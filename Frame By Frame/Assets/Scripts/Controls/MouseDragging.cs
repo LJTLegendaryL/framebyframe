c8f8ece53ed57b9eef95c36e33633aba51d77b49
eripo12 <eripo14@gmail.com>
2023-05-14 19:02:29 -0700

    Updated Menu and Sounds
    -Added background music and click SE
    -changed cursor to be skeletal hand
    -level transition

--- Frame By Frame/Assets/Scripts/Controls/MouseDragging.cs
+++ Frame By Frame/Assets/Scripts/Controls/MouseDragging.cs
@@ -2,37 +2,59 @@ using UnityEngine;
 
 public class MouseDragging : MonoBehaviour
 {
-	public Camera mainCamera;
-	public LayerMask interactableMask;
+    public Camera mainCamera;
+    public LayerMask interactableMask;
 
-	public Transform selectedObject;
-	bool selectableObjectInRange;
-	Vector2 mousePoint;
+    public Transform selectedObject;
+    bool selectableObjectInRange;
+    Vector2 mousePoint;
 
-	void Update()
-	{
-		CalculateMousePosition();
+    public AudioSource ding;
+    public AudioSource click;
 
-		if(selectedObject != null)
-			selectedObject.position = Vector3.Lerp(selectedObject.position, mousePoint, 10 * Time.deltaTime);
-	}
-	
-	void CalculateMousePosition()
-	{
-		mousePoint = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-		RaycastHit2D hitData2D = Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, interactableMask);
-		
-		if(selectedObject == null)
-		{
-			if(Input.GetMouseButtonDown(0) && hitData2D.transform != null)
-				selectedObject = hitData2D.transform;
-		}
-		else
-			if(Input.GetMouseButtonDown(0))
-				selectedObject = null;
-	}
-	
-	private void OnDrawGizmos() {
-		Gizmos.DrawRay(transform.position, mousePoint);
-	}
+
+    void Update()
+    {
+        CalculateMousePosition();
+
+        if (selectedObject != null)
+            selectedObject.position = Vector3.Lerp(selectedObject.position, mousePoint, 10 * Time.deltaTime);
+    }
+
+    private void getAudio()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        ding = GetComponent<AudioSource>();
+        click = GetComponent<AudioSource>();
+
+    }
+
+    void CalculateMousePosition()
+    {
+        mousePoint = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hitData2D = Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, interactableMask);
+
+        if (selectedObject == null)
+        {
+            if (Input.GetMouseButtonDown(0) && hitData2D.transform != null)
+            {
+                selectedObject = hitData2D.transform;
+                click.Play();
+            }
+
+
+        }
+        else
+            if (Input.GetMouseButtonDown(0))
+        {
+            selectedObject = null;
+            click.Play();
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, mousePoint);
+    }
 }
